7ddac35b8caefa6131d14e33275ebb194a343955
Hanran Liu <cocoarannie@163.com>
2014-09-11 15:23:15 +0800

    test

--- HRObserverDemo/HRObserverDemo/ViewController.m
+++ HRObserverDemo/HRObserverDemo/ViewController.m
@@ -41,16 +41,27 @@
     _user.sex  = @"man";
     
     self.userViewModel = [[UserViewModel alloc] initWithUser:_user];
-//    self.observer = [[HRKeyValueObserver alloc] initWithObject:_user
-//                                                   keyPathList:@[@"name", @"age", @"sex"]
-//                                                        target:self
-//                                                      selector:@selector(updateUserView)];
     NSDictionary *keyPathSelMap = @{@"name": NSStringFromSelector(@selector(refreshNameLabel)),
                                     @"age" : NSStringFromSelector(@selector(refreshAgeLabel)),
                                     @"sex" : NSStringFromSelector(@selector(refreshSexLabel))};
     self.observer = [[HRKeyValueObserver alloc] initWithObject:_user
                                             keyPathSelectorMap:keyPathSelMap
                                                         target:self];
+}
+
+- (void)testKeypathList
+{
+    self.observer = [[HRKeyValueObserver alloc] initWithObject:_user
+                                                   keyPathList:@[@"name", @"age", @"sex"]
+                                                        target:self
+                                                      selector:@selector(updateUserView)];
+}
+
+- (void)updateUserView
+{
+    self.sexLabel.text = self.userViewModel.userSex;
+    self.nameLabel.text = self.userViewModel.userName;
+    self.ageLabel.text = self.userViewModel.userAge;
 }
 
 - (void)refreshSexLabel
